f1d76821e84dfe3e29f9d58f90c1416e7c502323
Dan Bikle <danbikle@outlook.com>
2018-03-15 16:19:50 -0700

    more

---
+++ public/lesson005/workon_tkrs.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# workon_tkrs.bash
+
+# This script should subscribe to tkrs from GCP Pub/Sub.
+
+# After this script gets a tkr-string from Pub/Sub. it should "work-on" the tkr.
+
+# Demo:
+# bash workon_tkrs.bash
+
+cd ~
+python workon_tkrs.py
+
+exit
---
+++ public/lesson005/workon_tkrs.py
@@ -0,0 +1,11 @@
+#codeing utf-8
+"""
+workon_tkrs.py
+
+This script should help workon_tkrs.bash "work-on" tkrs from GCP Pub/Sub.
+
+Demo:
+python workon_tkrs.py
+"""
+
+'bye'
