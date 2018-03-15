8fd6c8e4f658e9b5a1b7080201581345ea94d842
Dan Bikle <danbikle@outlook.com>
2018-03-14 17:58:37 -0700

    more

--- public/lesson005/pubsub11.bash
+++ public/lesson005/pubsub11.bash
@@ -3,10 +3,18 @@
 # pubsub11.bash
 
 # Ref:
-#
+# https://console.cloud.google.com/iam-admin/serviceaccounts
+# https://cloud.google.com/pubsub/docs/quickstart-cli
 
+# Before I run this script I should,
+# create a service account via iam-admin/serviceaccounts page.
+# I should give pub/sub-admin privileges to the account.
+# When I create the account I should ask GCP to give me JSON file I later use to authenticate.
 
-# fail, use web: gcloud pubsub topics create topic11 sub11 --project pubsub-197323
+# Authentication demo:
+# gcloud auth activate-service-account --key-file=mykey.json
+
+gcloud pubsub topics create  topic11 sub11              --project pubsub-197323
 gcloud pubsub topics publish topic11 --message "hello0" --project pubsub-197323
 gcloud pubsub topics publish topic11 --message "hello1" --project pubsub-197323
 gcloud pubsub topics publish topic11 --message "hello2" --project pubsub-197323
