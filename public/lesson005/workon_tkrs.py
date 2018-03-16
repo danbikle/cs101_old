49700c4f4fa2c8ab2d986f7e175689269b48726b
Dan Bikle <danbikle@outlook.com>
2018-03-15 18:58:13 -0700

    more

--- public/lesson005/workon_tkrs.py
+++ public/lesson005/workon_tkrs.py
@@ -10,6 +10,7 @@ python workon_tkrs.py
 
 import subprocess as sp
 import re
+import pandas as pd
 
 cmd_s = 'gcloud pubsub subscriptions pull --auto-ack sub10 --project pubsub-197323'
 out_s = sp.run(cmd_s, shell=True, stdout=sp.PIPE)
@@ -24,8 +25,15 @@ I should match something like this:
 """
 
 out_l = out_s.stdout.decode('utf-8').split()
+# I should find the index of 'on'.
+# The tkr-index is one-plus the on-index:
 tkr_i = 1+out_l.index('on') # exception ok here
+tkr_s = out_l[tkr_i]
 print('tkr:')
-print(out_l[tkr_i])
+print(tkr_s)
+
+# I should use the tkr to get prices:
+
+price_df = pd.read_csv('https://cs101.herokuapp.com/lesson005/FB.csv')
 
 'bye'
