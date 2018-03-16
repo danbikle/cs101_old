f901d496a7392883f3122c98c501993c709aba7c
Dan Bikle <danbikle@outlook.com>
2018-03-15 17:54:56 -0700

    more

--- public/lesson005/workon_tkrs.py
+++ public/lesson005/workon_tkrs.py
@@ -8,4 +8,24 @@ Demo:
 python workon_tkrs.py
 """
 
+import subprocess as sp
+import re
+
+cmd_s = 'gcloud pubsub subscriptions pull --auto-ack sub10 --project pubsub-197323'
+out_s = sp.run(cmd_s, shell=True, stdout=sp.PIPE)
+
+"""
+I should match something like this:
+┌─────────────────────────┬────────────────┬────────────┐
+│           DATA          │   MESSAGE_ID   │ ATTRIBUTES │
+├─────────────────────────┼────────────────┼────────────┤
+│ Please work on AAPL now │ 55903378213755 │            │
+└─────────────────────────┴────────────────┴────────────┘
+"""
+
+out_l = out_s.stdout.decode('utf-8').split()
+tkr_i = 1+out_l.index('on') # exception ok here
+print('tkr:')
+print(out_l[tkr_i])
+
 'bye'
